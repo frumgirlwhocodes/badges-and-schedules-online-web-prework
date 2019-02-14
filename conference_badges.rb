181c7e9803a1e2ed3ac04fdacc15234e4cb8c9a6
lieby weintraub <liebahinda@gmail.com>
2019-02-14 18:05:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,4 +12,7 @@ def batch_badge_creator(array)
   def assign_rooms(array)
     new_array=[]
     array.each_with_index do |name,index|
-      new_array << "Hello, #{name}! You'll be assigned to room _____!"
+      new_array << "Hello, #{name}! You'll be assigned to room #{index}!"
+    end 
+    new_array
+  end 
